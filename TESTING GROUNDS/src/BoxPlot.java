cc3a43ede9456c387990227a6131cf0a275fd753
kngrace <kngrace@uw.edu>
2013-11-18 20:17:08 -0800

    I am attempting a commit from Eclipse now.

---
+++ TESTING GROUNDS/src/BoxPlot.java
@@ -0,0 +1,23 @@
+import javax.swing.JPanel;
+
+/*
+ * BoxPlot JPanel
+ */
+
+
+/**
+ * Creates a JPanel that accepts a Double array and draws
+ * a visual representation as a statistics BoxPlot.
+ * 
+ * @author Kirsten Grace
+ * @version 11.15.13
+ *
+ */
+
+
+public class BoxPlot extends JPanel {
+
+    
+    
+    
+}
